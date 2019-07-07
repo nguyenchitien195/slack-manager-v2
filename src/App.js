c94223c9286a6e009c2f426339c510d3adea9832
tien <nguyenchitien195@gmail.com>
2019-07-07 19:55:59 +0700

    Add function filter for user, channel, remove unused function

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Admin, Resource } from 'react-admin';
 import { UserList } from './users';
 import { ChannelList } from './channels';
 import { GroupList } from './groups';
-import { FileList, FileEdit, FileCreate } from './files';
+import { FileList } from './files';
 import UserIcon from '@material-ui/icons/Group';
 import FileIcon from '@material-ui/icons/FileUpload';
 import Dashboard from './Dashboard';
@@ -15,7 +15,7 @@ import MyLoginPage from './MyLoginPage';
 
 const App = () => (
   <Admin loginPage={MyLoginPage} dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
-    <Resource name="files" list={FileList} edit={FileEdit} create={FileCreate}  icon={FileIcon} />
+    <Resource name="files" list={FileList} icon={FileIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} />
     <Resource name="channels" list={ChannelList} icon={UserIcon} />
     <Resource name="groups" list={GroupList} icon={UserIcon} />
