56d912dfc6c18de25716f0a35545dc204c1fea80
tien <nguyenchitien195@gmail.com>
2019-08-06 13:37:15 +0700

    Updates

---
+++ precache-manifest.6697f6e283c38d547e7cf65ff1589390.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7896e2aaf97c175313db35d3aeacef",
+    "url": "/slack-manager-v2/index.html"
+  },
+  {
+    "revision": "f7b6a5778e3f34a8e44e",
+    "url": "/slack-manager-v2/static/css/main.a7ec14da.chunk.css"
+  },
+  {
+    "revision": "9eeba0717fc48218ca94",
+    "url": "/slack-manager-v2/static/js/2.6a0ed31b.chunk.js"
+  },
+  {
+    "revision": "f7b6a5778e3f34a8e44e",
+    "url": "/slack-manager-v2/static/js/main.e76e5539.chunk.js"
+  },
+  {
+    "revision": "c04df031ade882b9b736",
+    "url": "/slack-manager-v2/static/js/runtime~main.c9d1c6b3.js"
+  }
+]);
